1e1bebc4fd0f044fee00ad8e6cb16182831378d5
Traveller_314 <traveller31415@gmail.com>
2023-04-20 22:42:07 +0800

    date analyze and handle dataframe

--- get_close_price_demo.py
+++ get_close_price_demo.py
@@ -4,7 +4,7 @@ import io
 import pandas as pd
 import time
 
-fileName = 'KlineData.csv'
+fileName = 'SpotKlineData.csv'
 
 
 def get_close(link):
@@ -17,7 +17,6 @@ def get_close(link):
                 content = file.read()
     
     content = str(content).strip("b'\\n'")
-    print(content)
     arr = [x.strip() for x in content.split(",")]
     arr = [int(arr[0])] + [float(x) for x in arr[1:]]
     
@@ -27,12 +26,10 @@ if __name__ == '__main__':
     # 获取当前时间戳
     timestamp = time.time()-1*24*60*60
     date = time.strftime('%Y-%m-%d', time.gmtime(timestamp))
-    spotUrl = 'https://data.binance.vision/data/spot/daily/klines/BETHUSDT/1d/BETHUSDT-1d-%s.zip'%(date) #be like this '2023-04-19'
-    futureUrl = 'https://data.binance.vision/data/futures/um/daily/klines/ETHUSDT/1d/ETHUSDT-1d-%s.zip'%(date)
-    
-    '''content = get_close(spotUrl)
+    zipfileurl = 'https://data.binance.vision/data/spot/daily/klines/BETHUSDT/1d/BETHUSDT-1d-%s.zip'%(date) #be like this '2023-04-19'
+    content = get_close(zipfileurl)
     
     csv_file = pd.read_csv(fileName)
     dictContent = {k: v for k, v in zip(list(csv_file.columns), content)}
     csv_file = pd.concat([csv_file, pd.DataFrame(dictContent,index=[0])]).drop_duplicates(subset=['Opentime'])
-    csv_file.to_csv(fileName,index=False)'''
+    csv_file.to_csv(fileName,index=False)
