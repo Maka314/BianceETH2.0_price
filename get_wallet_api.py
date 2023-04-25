b6887386658ba5d6989343b7d110f3f4a0c8fe24
Traveller_314 <traveller31415@gmail.com>
2023-04-25 23:54:50 +0800

    some modification

--- get_wallet_api.py
+++ get_wallet_api.py
@@ -23,6 +23,13 @@ c_future_balance = pd.DataFrame(future_client.balance())
 spot_client.ticker_price(symbol='BETHUSDT')
 future_client.get_income_history()'''
 
+def timestamp_transfer(timestamp):
+    if timestamp // 1000000000000:
+        dt = datetime.datetime.fromtimestamp(timestamp/1000)
+    else:
+        dt = datetime.datetime.fromtimestamp(timestamp)
+    return("时间戳 {} 对应的时间是：{}".format(timestamp, dt))
+
 def spot_latest_hold(spot_snapshot, asset_name):
     current_hold = pd.DataFrame(spot_snapshot['snapshotVos'][-1]['data']['balances'])
     current_timestamp = spot_snapshot['snapshotVos'][-1]['updateTime']
@@ -30,8 +37,19 @@ def spot_latest_hold(spot_snapshot, asset_name):
     asset_hold['timestamp'] = pd.Series([current_timestamp]*len(asset_hold))
     return asset_hold
 
+def future_pickup(c_future_balance, asset):
+    targetasset = c_future_balance[c_future_balance['asset']==asset]
+    future_net_value = float(targetasset['balance'].values[0]) + float(targetasset['crossUnPnl'].values[0])
+    future_timestamp = targetasset['updateTime'].values[0]
+    return future_net_value, future_timestamp
+
 if __name__ == '__main__':
     nv_data = pd.read_csv(nv_file)
     snapshot = spot_client.account_snapshot(type='SPOT')
-    beth_held = spot_latest_hold(snapshot,'BETH')
-    pass
+    spot_held = spot_latest_hold(snapshot,'BETH')
+    spot_held['ticker_price'] = spot_client.ticker_price(symbol='BETHUSDT')['price']
+    spot_net_value = float(spot_held['free'][0])*float(spot_held['ticker_price'][0])
+
+    c_future_balance = pd.DataFrame(future_client.balance())
+    future_net_value, future_timestamp = future_pickup(c_future_balance, 'USDT')
+    
